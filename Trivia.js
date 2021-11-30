c738cf5ad45783ef285f726bf322f4eb95971553
Mitchell Hurley <mdhurley@pugetsound.edu>
2021-11-29 17:10:52 -0800

    initial trivia scripting and outline

--- Trivia.js
+++ Trivia.js
@@ -1,7 +1,10 @@
 const axios = require("axios");
 let categories = document.querySelector("#categories")
-let questions = 10;
 
+//TODO Add button to start game and event listener for that
+//TODO Once game starts, make new html elements for the options, including 3(?) incorrect options and the correct but randomized order
+//TODO Give them a submit answer button that checks the selection is the same as the JSON objects correct answer
+//TODO Display basic Congratulations message
 
 //Function that grabs the questions of a random category from the api and returns a JSON object 
 async function getQuestionsAny(){
