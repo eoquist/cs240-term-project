784364e2b49129a37f1271fa61693840188e3f4d
Madison Sanchez-Forman <msanchezforman@pugetsound.edu>
2021-11-30 11:08:56 -0800

    linked two html pages together

--- Trivia.js
+++ Trivia.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 let categories = document.querySelector("#categories")
-
+let questions = 10;
 //TODO Add button to start game and event listener for that
 //TODO Once game starts, make new html elements for the options, including 3(?) incorrect options and the correct but randomized order
 //TODO Give them a submit answer button that checks the selection is the same as the JSON objects correct answer
--- elements.js
+++ elements.js
@@ -11,48 +11,19 @@ if(!door_open){
     return;
 }
 if(door.getAttribute("src") == "images/door-close.png"){
-door.className = "door-open";
 door.src = "images/door-open.png";
 door.style.position = "absolute";
 door.style.left = "350px";
 door.style.top = "180px"
-make_table();
-game_start = true;
-} else if(!game_start){
-    door.className = "door-closed";
-    door.src = "images/door-close.png";
-    door.style.position = "absolute";
-    door.style.left = "600px";
-    door.style.top = " 208px";
-}
+} else{
+location.pathname = "trivia.html";
+//     door.src = "images/door-close.png";
+//     door.style.position = "absolute";
+//     door.style.left = "600px";
+//     door.style.top = " 208px";
+ }
 })
-<<<<<<< HEAD
-function make_table(){
-    var tbl = document.createElement("table");
-    tbl.style.width = '100px';
-  tbl.style.border = '1px solid black';
-  for (let i = 0; i < 3; i++) {
-    const tr = tbl.insertRow();
-    for (let j = 0; j < 5; j++) {
-      if (i === 2 && j === 1) {
-        break;
-      } else {
-        const td = tr.insertCell();
-        td.appendChild(document.createTextNode(`Cell I${i}/J${j}`));
-        td.style.border = '1px solid black';
-        if (i === 1 && j === 1) {
-          td.setAttribute('rowSpan', '2');
-        }
-      }
-    }
-}
-document.body.appendChild(tbl);
-tbl.style.position = "absolute";
-tbl.style.left = "500px";
-tbl.style.top = "180px"
-tbl.style.backgroundColor = "white";
-}
-=======
+
 
 // Paper that rips one time if you click on it
 onetime(document.getElementById("paper1"), "click", "icons/paper1-ripped.png");
@@ -65,4 +36,3 @@ function onetime(node, type, imgToSwap) { // create a one-time event
 		e.target.removeEventListener(e.type, arguments.callee); // remove event
 	});
 }
->>>>>>> eadd9f8b7a2365aad86190f82852dbda9ea3ee5a
