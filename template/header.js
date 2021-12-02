bd3aeb8fd339a6ffb2783939e0c3af892c5c2711
Emilee Oquist <emiwemi495@gmail.com>
2021-12-02 15:01:45 -0800

    click and drag thing

--- template/header.js
+++ template/header.js
@@ -36,3 +36,35 @@ function getConfirmation(alertbox, confirmQ, confirmed, denied) {
        return false;
     }
  }
+
+ // click and hold to drag dragable elements function
+var moving = false;
+
+dog.addEventListener("mousedown", initialClick, false);
+cat.addEventListener("mousedown", initialClick, false);
+
+
+function move(e){
+  var newX = e.clientX - 10;
+  var newY = e.clientY - 10;
+
+  image.style.left = newX + "px";
+  image.style.top = newY + "px";
+
+  
+}
+
+function initialClick(e) {
+
+  if(moving){
+    document.removeEventListener("mousemove", move);
+    moving = !moving;
+    return;
+  }
+  
+  moving = !moving;
+  image = this;
+
+  document.addEventListener("mousemove", move, false);
+
+}
