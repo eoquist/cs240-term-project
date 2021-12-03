30e11bda4c8bb9ffde0d20d25261c01fc243d8af
Emilee Oquist <emiwemi495@gmail.com>
2021-12-02 20:04:49 -0800

    start on tooltip text

--- template/header.js
+++ template/header.js
@@ -1,4 +1,20 @@
- // taken from https://www.sitepoint.com/create-one-time-events-javascript/
+document.getElementById("door").addEventListener("click", function(){
+    let node = document.getElementById("door");
+    let src1 = "/images/door-close.png";
+    let src2 = "/images/door-openv4.png";
+        if(node.getAttribute("src") == src1){
+            node.src = src2; // set to open
+            node.style.setProperty("height","590px");
+            node.style.setProperty("left","563.5px");
+            node.style.setProperty("top","193px");
+        }
+        else{
+            node.src = src1; // close
+            node.style.setProperty("height","512px");
+            node.style.setProperty("left","570px");
+            node.style.setProperty("top","208px");
+        } 
+});// taken from https://www.sitepoint.com/create-one-time-events-javascript/
  function onetime(node, type, srcToSwap) { // create a one-time event
     node.addEventListener(type, function(e) { // create event
         e.target.src = srcToSwap;
@@ -37,34 +53,34 @@ function getConfirmation(alertbox, confirmQ, confirmed, denied) {
     }
  }
 
+ /**
+  * Functions that allow a user to click and move an image around the screen
+  * Code snippets taken from:
+  * Endless's post --> https://stackoverflow.com/questions/33948464/move-an-image-with-javascript-using-mouse-events
+  */
  // click and hold to drag dragable elements function
-var moving = false;
+        var moving = false;
+        const draggableElements = document.querySelectorAll(".draggable");
+        for (const element of draggableElements) { // apply eventListener to all elements returned by the querySelector above
+        element.addEventListener("mousedown", initialClick, false);
+        }
 
-dog.addEventListener("mousedown", initialClick, false);
-cat.addEventListener("mousedown", initialClick, false);
+        function move(e){
+        var newX = e.clientX - 10;
+        var newY = e.clientY - 10;
+        // TODO: If element ends up out of bounds, return to a default position??
+        image.style.left = newX + "px";
+        image.style.top = newY + "px";
+        }
 
-
-function move(e){
-  var newX = e.clientX - 10;
-  var newY = e.clientY - 10;
-
-  image.style.left = newX + "px";
-  image.style.top = newY + "px";
-
-  
-}
-
-function initialClick(e) {
-
-  if(moving){
-    document.removeEventListener("mousemove", move);
-    moving = !moving;
-    return;
-  }
-  
-  moving = !moving;
-  image = this;
-
-  document.addEventListener("mousemove", move, false);
-
-}
+        function initialClick(e) {
+        if(moving){
+            document.removeEventListener("mousemove", move);
+            moving = !moving;
+            return;
+        }
+        moving = !moving;
+        image = this;
+        document.addEventListener("mousemove", move, false);
+        }
+        // TODO: LIMIT RANGE OF DRAGGABLE ITEMS TO SPECIFIC AREAS AND CHANGE THEIR Z-INDEX
